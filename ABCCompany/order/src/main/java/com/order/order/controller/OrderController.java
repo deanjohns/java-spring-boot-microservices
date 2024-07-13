4242ea505ffaf8b4d91775d09d22237198bfcf40
deanjohns <srimal.git@gmail.com>
2024-07-14 03:31:43 +0530

    integrated kafka

--- ABCCompany/order/src/main/java/com/order/order/controller/OrderController.java
+++ ABCCompany/order/src/main/java/com/order/order/controller/OrderController.java
@@ -1,7 +1,9 @@
 package com.order.order.controller;
 
+import com.base.base.dto.OrderEventDTO;
 import com.order.order.common.OrderResponse;
 import com.order.order.dto.OrderDTO;
+import com.order.order.kafka.OrderProducer;
 import com.order.order.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +17,9 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+    @Autowired
+    private OrderProducer orderProducer;
+
     @GetMapping("/getorders")
     public List<OrderDTO> getOrders() {
         return orderService.getAllOrders();
@@ -27,6 +32,11 @@ public class OrderController {
 
     @PostMapping("/addorder")
     public OrderResponse saveOrder(@RequestBody OrderDTO orderDTO) {
+        OrderEventDTO orderEventDTO = new OrderEventDTO();
+        orderEventDTO.setMessage("Order is committed");
+        orderEventDTO.setStatus("pending");
+        orderProducer.sendMessage(orderEventDTO);
+
         return orderService.saveOrder(orderDTO);
     }
 
---
+++ ABCCompany/order/src/main/java/com/order/order/dto/OrderEventDTO.java
@@ -0,0 +1,13 @@
+package com.order.order.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderEventDTO {
+    private String message;
+    private String status;
+}
